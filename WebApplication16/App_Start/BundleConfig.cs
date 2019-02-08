3ee3b88806e0786659948033032be64611e2c3f9
programinurdu <aasdb@progd23.com>
2019-02-08 10:41:35 +0000

    Add project files.

---
+++ WebApplication16/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication16.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Modernizr
+            bundles.Add(new ScriptBundle("~/modernizr/js").Include(
+                        "~/Scripts/modernizr-2.8.3.js"
+                ));
+
+            // Javascript
+            bundles.Add(new ScriptBundle("~/bootstrap/js").Include(
+                        "~/Scripts/jquery-3.3.1.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/Site.js"
+                ));
+
+            // CSS
+            bundles.Add(new StyleBundle("~/bootstrap/css").Include(
+                        "~/Content/Site.css",
+                        "~/Content/bootstrap.css",
+                        "~/Content/fontaweson-all.css"
+               ));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
